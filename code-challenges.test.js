c9e9ceca7b3cb4e97ad86167689d531e849d562d
henrike32 <henrique.lambertucci@gmail.com>
2023-05-05 20:07:27 -0400

    did 1st challenge,taking a break probably will code later

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -20,7 +20,44 @@ const fibLength1 = 6
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+// describe('fibNum', () => {
+//   it("gives six Fibonacci numbers in an array", () => {
+//     expect(fibNum(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+//     expect(fibNum(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+//   })
+// })
+// ReferenceError: fibNum is not defined
 // b) Create the function that makes the test pass.
+
+// pseudocode ->
+// input - number
+// output - array with the desired Fibonacci number
+
+// process ->
+// create a function named 'fibNum' 
+// use conditionals statements to setup the base arguments
+// for loop  is going to be used to iterate through an array 
+// create a variable named 'fibArray 'with specified values to make the final result possible
+// the .push() built in method to push the desired numbers into 'fibArray'
+// 
+const fibNum= (n) => {
+  if (n <= 0){
+    return [];
+  } else if ( n === 1){
+    return [1];
+  } else if ( n === 2){
+    return [1, 1];
+  } else {
+    const fibArray = [1, 1];
+    for (let i = 2; i < n; i++) {
+      fibArray.push(fibArray[i-1]+fibArray[i-2]);
+    }
+    return fibArray;
+  }}
+  console.log(fibNum(fibLength1));
+  console.log(fibNum(fibLength2));
+
+  // output: [ 1, 1, 2, 3, 5, 8 ] and [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
